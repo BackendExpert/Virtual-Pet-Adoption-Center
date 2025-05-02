805eb324f9c015b66f28ca4b611071eba62e7088
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-02 19:51:57 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,23 @@
+// required packages
 const express = require('express');
 const cors = require('cors');
-const ConnectDB = require('./config/DB')
 require('dotenv').config();
 
+// database connection file
+const ConnectDB = require('./config/DB')
 
+// routes
+const petRoute = require('./routes/petRoute')
+
+// call for database connection fuction
 ConnectDB()
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+app.use('/pet', petRoute)
 
 
 module.exports = app
