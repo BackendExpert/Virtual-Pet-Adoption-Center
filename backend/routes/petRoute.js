63b50d087e3a62f433c2ec3790226f2d4418528e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-02 21:17:21 +0530

    Update petRoute.js

--- backend/routes/petRoute.js
+++ backend/routes/petRoute.js
@@ -3,23 +3,29 @@ const petController = require('../controllers/petController');
 
 const router = express.Router();
 
+// in app.js file i use
+// app.use('/pet', petRoute)
+
+// so i not use pet in here
+
 // create pet
-router.post('/pet', petController.createPet)
+router.post('/', petController.createPet)
 
 // get all pets
-router.get('/pet', petController.allpets)
+router.get('/', petController.allpets)
 
 // get single pet
-router.get('/pet/:id', petController.getSinglePet)
+router.get('/:id', petController.getSinglePet)
 
 // update pet
-router.put('/pet/:id', petController.updatePet)
+router.put('/:id', petController.updatePet)
 
 // Adopt a Pet
-router.patch('/pet/:id/adopt', petController.petAdopt)
+router.patch('/:id/adopt', petController.petAdopt)
 
 // delete pet
-router.delete('/pet/:id', petController.deletePet)
+router.delete('/:id', petController.deletePet)
+
 
 
 module.exports = router;
