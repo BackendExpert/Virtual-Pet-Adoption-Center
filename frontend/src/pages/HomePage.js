b19c41a847d9a1eda7dca0eb093b884307fd3d36
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-02 12:06:36 +0530

    UPdate

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,29 @@
+// src/pages/HomePage.js
+import React from 'react';
+
+import '../styles/global.css';
+
+const HomePage = () => {
+
+  return (
+    <div className="container">
+      <h1 className="title">🐾 Virtual Pet Adoption Center</h1>
+
+      <div className="top-bar">
+
+        <button
+          className="add-button"
+
+        >
+            hi all
+        </button>
+      </div>
+
+
+
+
+    </div>
+  );
+};
+
+export default HomePage;
