ab321aae59436bcfcac2dd8fbbe292d08b1d874d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-03 14:07:52 +0530

    Update PetCard.jsx

--- frontend/src/components/PetCard.jsx
+++ frontend/src/components/PetCard.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getOnePet } from '../services/api'
+import { formatDate } from '../utils/Helper';
 
 const PetCard = ({ petId }) => {
-  return (
-    <div className='bg-white p-8 rounded-xl shadow-xl my-4'>
-        {petId}
-    </div>
-  )
+    const [pet, setPet] = useState([]);
+
+    useEffect(() => {
+        const fetchPet = async () => {
+            try {
+                const res = await getOnePet(petId);
+                setPet(res.data.Result);
+            } catch (err) {
+                setError('Failed to fetch pet data');
+            }
+        };
+
+        if (petId) fetchPet();
+    }, [petId]);
+
+    return (
+        <div className='bg-white p-8 rounded-xl shadow-xl my-4'>
+            <div className='text-gray-500'>
+                <h2 className='text-2xl font-bold mb-2 '>{pet?.name}</h2>
+                <p className='mb-1'><span className='font-semibold'>Age:</span> {pet?.age}</p>
+                <p className='mb-1'><span className='font-semibold'>Personality:</span> {pet?.personality}</p>
+                <p className='mb-1'><span className='font-semibold'>Species:</span> {pet?.species}</p>
+                <p className='mb-1'><span className='font-semibold'>Mood:</span> {pet?.mood}</p>
+
+                <div className="">
+                    {
+                        pet?.adopted === true ? 
+                        <div className="text-red-500 font-semibold">Adopted</div>
+                        :
+                        <div className="text-green-500 font-semibold">Not Adopted</div>
+                    }
+                </div>
+
+                <div className="">
+                    {
+                        pet?.adopted === true ? 
+                        <div className="text-blue-500 font-semibold">{formatDate(pet?.adoption_date)}</div>
+                        :
+                        <div className=""></div>
+                    }
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default PetCard
